f8ff31cd732a65c1540bfa6fa0d89e7b8680e070
Rafaelmkp <rafaelmobarack@gmail.com>
2020-09-27 17:14:25 -0300

    db script

---
+++ db_files/DB_IAD_producao.sql
@@ -0,0 +1,63 @@
+create database processos;
+
+--table publicacao nao-classif
+CREATE TABLE processos.publicacao_unirriter (
+	pub_id bigserial NOT NULL,
+	pub_cnj_id int8 NOT NULL DEFAULT 1,
+	pub_cnj bool NOT NULL DEFAULT false,
+	pub_numero_processo varchar NULL,
+	pub_numerocnj jsonb NULL,
+	pub_tj_id int8 NULL,
+	pub_tj_descricao varchar NULL,
+	pub_jor_id int8 NULL,
+	pub_jor_edicao int8 NULL,
+	pub_jor_data_publicacao date NULL,
+	pub_pagina int4 NULL,
+	pub_orgao varchar NULL,
+	pub_vara varchar NULL,
+	pub_cidade varchar NULL,
+	pub_estado varchar NULL,
+	pub_hash varchar NOT NULL,
+	pub_conteudo text NOT NULL,
+	pub_conteudo_tsvector tsvector NULL,
+	"language" text NOT NULL DEFAULT 'portuguese'::text,
+	pub_data_processado timestamptz NULL DEFAULT now(),
+	pub_conteudo_tratado text NULL,
+	advogado_mapeado bool NULL,
+	advogado_mapeado_erro bool NULL,
+	pub_cnj_revisado bool NULL,
+	CONSTRAINT pk_publicacao_unirriter PRIMARY KEY (pub_id)
+);
+
+--table publicacoes classific
+
+--table advogado
+create table processos.advogado (
+	adv_id serial not null,
+	adv_nome varchar(64)not null,
+	adv_oab varchar(32)null,	
+	constraint pk_advogado primary key(adv_id)
+);
+
+--table publicacao advogado nxm
+create table publicacao_advogado (
+	pub_adv_id bigserial not null,
+	adv_id int not null,
+	pub_id bigint not null
+);
+
+--tabela reu eh necessaria??
+--table reu
+create table reu
+
+--table publicacao reu nxm
+
+--table natureza processual
+
+--tipo de decisao
+
+--table peça a produzir
+
+--procedure busca pub_nao_classif
+
+--procedure salva pub_classif
