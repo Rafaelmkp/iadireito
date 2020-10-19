1c9f821dd5ba6679dd845cdbd61cfa9e0b5e5e92
Rafaelmkp <rafaelmobarack@gmail.com>
2020-10-19 19:33:08 -0300

    teste ddl definitivo do banco

--- db_files/script_db_test.sql
+++ db_files/script_db_test.sql
@@ -1,31 +1,11 @@
-create table tb_pub(
-	pub_id	integer primary key not null,
-	pub_cnj_id integer,	
-	pub_cnj	varchar(10),
-	pub_numero_processo	varchar(64),
-	pub_numerocnj varchar(256),
-	pub_tj_id smallint,
-	pub_tj_descricao varchar,
-	pub_jor_id integer,	
-	pub_jor_edicao smallint,	
-	pub_jor_data_publicacao	date,
-	pub_pagina smallint,	
-	pub_orgao varchar(128),
-	pub_vara smallint,
-	pub_cidade varchar(64),
-	pub_estado varchar(2),
-	pub_hash varchar,
-	pub_conteudo varchar,
-	pub_conteudo_tsvector varchar,	
-	language varchar(32),
-	pub_data_processado date,
-	pub_conteudo_tratado varchar,
-	advogado_mapeado varchar(64),
-	advogado_mapeado_erro varchar,
-	pub_cnj_revisado boolean
-);
+drop database if exists IADireito;
 
-CREATE TABLE publicacao_unirriter (
+create database processos;
+
+create schema processos;
+
+--table publicacao nao-classificadas
+CREATE TABLE processos.publicacao_unirriter (
 	pub_id bigserial NOT NULL,
 	pub_cnj_id int8 NOT NULL DEFAULT 1,
 	pub_cnj bool NOT NULL DEFAULT false,
@@ -52,42 +32,87 @@ CREATE TABLE publicacao_unirriter (
 	pub_cnj_revisado bool NULL,
 	CONSTRAINT pk_publicacao_unirriter PRIMARY KEY (pub_id)
 );
-
+--table controle classificacao
+create table processos.controle (
+	controle_id bigserial not null,
+	pub_n_classif_id bigint not null,
+	leitura boolean default false,
+	classificado boolean default false,
+	constraint pk_controle primary key (controle_id),
+	constraint fk_pub_n_classif 
+		foreign key (pub_n_classif_id) 
+			references processos.publicacao_unirriter(pub_id)
+)
 --table advogado
-create table advogado (
+create table processos.advogado (
 	adv_id serial not null,
 	adv_nome varchar(64)not null,
-	adv_oab varchar(32)null,	
+	adv_oab varchar(32)null,
+	adv_estado char(2)null,
 	constraint pk_advogado primary key(adv_id)
 );
-
 --table publicacao advogado nxm
-create table publicacao_advogado (
+create table processos.publicacao_advogado (
 	pub_adv_id bigserial not null,
-	adv_id int not null references advogado(adv_id),
-	pub_id bigint not null references publicacao_unirriter(pub_id),
-	constraint pk_publicacao_advogado primary key(pub_adv_id)
+	adv_id serial not null,
+	pub_clas_id bigserial not null,
+	constraint pk_publicacao_advogado primary key(pub_adv_id),
+	constraint fk_advogado 
+		foreign key(adv_id) 
+			references processos.advogado(adv_id),
+	constraint fk_publicacoes_classificadas 
+		foreign key(pub_clas_id) 
+			references processos.publicacoes_classificadas(pub_clas_id)
 );
-
-create table natureza (
+--table natureza processual
+create table processos.natureza_processual (
 	nat_id serial not null,
-	nat_value varchar(32) not null,
-	nat_desc varchar(32) not null,
-	constraint pk_natureza primary key(nat_id)
+	nat_descricao varchar(32) not null,
+	constraint pk_natureza_processual primary key(nat_id)
 );
-
-create table tipo_decisao (
-	tp_id serial not null,
-	tp_value varchar(64) not null,
-	tp_desc varchar(64),
-	constraint pk_tipo_decisao primary key(tp_id)
+--table tipo de decisao
+create table processos.decisao_tipo (
+	dec_id serial not null,
+	dec_descricao varchar(32) not null,
+	constraint pk_decisao_tipo primary key(dec_id)
 );
-
-create table peca_produzir (
+--table peca a produzir
+create table processos.peca_produzir (
 	pec_id serial not null,
-	pec_value varchar(64),
-	pec_desc varchar(64),
+	pec_descricao varchar(32) not null,
 	constraint pk_peca_produzir primary key(pec_id)
+);
+--table partes autor ou reu
+create table processos.partes (
+	partes_id bigserial not null,
+	nome varchar(32) not null,
+	parte varchar(32) not null,
+	pub_clas_id bigserial not null,
+	constraint pk_partes primary key(partes_id),
+	constraint fk_publicacoes_classificadas 
+		foreign key(pub_clas_id) 
+			references processos.publicacoes_classificadas(pub_clas_id)
+);
+--table publicacoes classificadas
+create table processos.publicacoes_classificadas (
+	pub_clas_id bigserial not null,
+	estrutura varchar(32),
+	numero_cnj int8,
+	numero_processo varchar(32),
+	natureza_processual varchar(32),
+	vara varchar(32),
+	estado char(2),
+	comarca varchar(32),
+	juiz varchar(32),
+	decisao_tipo varchar(32),
+	peca_produzir varchar(32),
+	inicio_prazo date,
+	prazo int,
+	dias_corridos boolean,
+	dias_uteis boolean,
+	fim_prazo date,
+	ha_custas boolean,
+	constraint pk_publicacoes_classificadas primary key(pub_clas_id)
 );
 
 insert into natureza 
--- views_cache/formulario.5392d8c38185bfa8023dbfb235e724fd.rtpl.php
+++ views_cache/formulario.5392d8c38185bfa8023dbfb235e724fd.rtpl.php
@@ -1,49 +1,128 @@
 <?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8" content="width=device-width, initial-scale=1.0"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet"   href="../res/style/style.css">
-</head>
-<body>
+  <head>
+    <meta charset="utf-8" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-  <form action="/submit-summons" method="post" autocomplete="off">
-    <div class="parent">
+    <link rel="stylesheet" href="./res/style/style.css" />
+    <link
+      href="ContatoEstilo.css"
+      rel="stylesheet"
+      type="text/css"
+      media="print"
+    />
+    <link
+      rel="stylesheet"
+      href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css"
+    />
+    <link rel="stylesheet" href="../res/style/style.css" />
+
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  </head>
+
+  <body>
+    <form action="/submit-summons" method="post" autocomplete="off">
       <h1 align="center">Dados Processuais</h1>
-      <div>
-        <table width="100%" id="dados">
-          <tr>
-            <td><label for="estrutura">Estrutura processual:</label></td>
-            <td><select name="estrutura" id="estrutura">
-                <option value="branco"> </option>
-                <option value="federal">Federal</option>
-                <option value="estadual">Estadual</option>
-              </select></td>
-          </tr>
-          <tr>
-            <td><label for="n_processo">Número CNJ:</label></td>
-            <td><input type="text" id="n_cnj" name="n_cnj" size="20" maxlength="20"></td>
-          </tr>
-          <tr>
-            <td><label for="n_processo">Processo número:</label></td>
-            <td><input type="text" id="n_processo" name="n_processo"></td>
-          </tr>
-          <tr>
-            <td><label for="natureza">Natureza:</label></td>
-            <td><select name="natureza" id="natureza">
-              <?php $counter1=-1;  if( isset($natureza) && ( is_array($natureza) || $natureza instanceof Traversable ) && sizeof($natureza) ) foreach( $natureza as $key1 => $value1 ){ $counter1++; ?>
-                <option value="<?php echo htmlspecialchars( $value1["nat_value"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["nat_desc"], ENT_COMPAT, 'UTF-8', FALSE ); ?> </option>
-              <?php } ?>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td><label name="vara">Vara:</label></td>
-            <td><input type="text" id="vara" name="vara"></td>
-          </tr>
-          <tr>
-            <td><label for="vara">Estado:</label></td>
-            <td><select name="estado" id="estado" onchange="buscaCidades(this.value)">
+      <br />
+      <div class="parent">
+        <div class="conteudo">
+          <label for="conteúdo">Nota de expediente:</label>
+          <br /><br />
+          <textarea
+            id="conteudo"
+            name="conteudo"
+            rows="45"
+            cols="50"
+            align="center"
+          >
+<?php echo htmlspecialchars( $summons["conteudo"], ENT_COMPAT, 'UTF-8', FALSE ); ?></textarea
+          >
+        </div>
+
+        <div class="dados">
+          <label for="estrutura">Estrutura:</label>
+          <select name="estrutura" id="estrutura">
+            <option value="branco"></option>
+            <option value="federal">Federal</option>
+            <option value="estadual">Estadual</option>
+          </select>
+          <br /><br />
+          <label for="n_processo">Número CNJ:</label>
+          <input type="text" id="n_cnj" name="n_cnj" size="20" maxlength="20" />
+          <br /><br />
+          <label for="n_processo">Número do processo:</label>
+          <input type="text" id="n_processo" name="n_processo" />
+          <br /><br />
+          <label for="natureza">Natureza:</label>
+          <select name="natureza" id="natureza">
+            <?php $counter1=-1;  if( isset($natureza) && ( is_array($natureza) || $natureza instanceof Traversable ) && sizeof($natureza) ) foreach( $natureza as $key1 => $value1 ){ $counter1++; ?>
+            <option value="<?php echo htmlspecialchars( $value1["nat_value"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["nat_desc"], ENT_COMPAT, 'UTF-8', FALSE ); ?></option>
+            <?php } ?>
+          </select>
+          <br /><br />
+          <label name="vara">Vara:</label>
+          <input type="text" id="vara" name="vara" maxlength="2" size="3" />
+          <br /><br />
+          <label for="vara">Estado:</label>
+          <select name="estado" id="estado" onchange="buscaCidades(this.value)">
+            <option value="branco"></option>
+            <option value="AC">AC</option>
+            <option value="AL">AL</option>
+            <option value="AP">AP</option>
+            <option value="AM">AM</option>
+            <option value="BA">BA</option>
+            <option value="CE">CE</option>
+            <option value="DF">DF</option>
+            <option value="ES">ES</option>
+            <option value="GO">GO</option>
+            <option value="MA">MA</option>
+            <option value="MT">MT</option>
+            <option value="MS">MS</option>
+            <option value="MG">MG</option>
+            <option value="PA">PA</option>
+            <option value="PB">PB</option>
+            <option value="PR">PR</option>
+            <option value="PE">PE</option>
+            <option value="PI">PI</option>
+            <option value="RJ">RJ</option>
+            <option value="RN">RN</option>
+            <option value="RS">RS</option>
+            <option value="RO">RO</option>
+            <option value="RR">RR</option>
+            <option value="SC">SC</option>
+            <option value="SP">SP</option>
+            <option value="SE">SE</option>
+            <option value="TO">TO</option>
+          </select>
+          <br /><br />
+          <label for="vara">Comarca:</label>
+          <select id="cidade" name="cidade"></select>
+          <br /><br />
+          <label for="juiz">Juiz:</label>
+          <input type="text" id="juiz" name="juiz" maxlength="30" size="30" />
+          <br /><br />
+          <div id="autor">
+            <div id="TextBoxesGroup">
+              <div id="TextBoxDiv1">
+                <label>Autor: </label><input type="textbox" id="autor" />
+              </div>
+            </div>
+            <input type="button" value="Adicionar" id="addAutor" />
+            <input type="button" value="Remover" id="removeAutor" />
+          </div>
+          <br /><br />
+          <div class="input_fields_wrap">
+            <button class="add_field_button">Adicionar</button>
+            <br /><br />
+            <div>
+              <label for="advautor">Advogado:</label
+              ><input type="text" name="mytext[]" /><br /><br />
+              <label for="oabadvautor">OAB:</label>
+              <input type="text name=oabadvautor" /><br /><br />
+              <label for="estoabautor">Estado:</label>
+              <select name="estoabautor" id="estoabautor">
                 <option value="branco"></option>
                 <option value="AC">AC</option>
                 <option value="AL">AL</option>
@@ -73,32 +152,29 @@
                 <option value="SE">SE</option>
                 <option value="TO">TO</option>
               </select>
-            </td>
-          </tr>
-          <tr>
-            <td><label for="vara">Comarca:</label></td>
-            <td><select id="cidade" name="cidade"><select></td>
-          </tr>
-          <tr>
-            <td><label for="juiz">Juiz:</label></td>
-            <td><input type="text" id="juiz" name="juiz"></td>
-          </tr>
-          <tr>
-            <td><label for="autor">Autor:</label></td>
-            <td><input type="text" id="autor" name="autor"></td>
-          </tr>
-          <!--aqui tr para o botão de inserir adv-->
-            <tr id="advbtn">
-              <td></td>
-              <td>
-                <input type=button id="addAdv" value="Add Advogado">
-                <input type=button id="excludeAdv" value="Excluir Advogado">
-              </td>
-            </tr>
-          <!--fim tr botoes adv-->
-          <tr>
-            <td><label for="estado1">Estado:</label></td>
-            <td><select name="estado1" id="estado1">
+            </div>
+          </div>
+          <br /><br />
+          <div id="reu">
+            <div id="TextGroup">
+              <div id="TextDiv1">
+                <label>Réu: </label><input type="textbox" id="reu" />
+              </div>
+            </div>
+            <input type="button" value="Adicionar" id="addReu" />
+            <input type="button" value="Remover" id="removeReu" />
+          </div>
+          <br /><br />
+          <div class="input_fields_wrapA">
+            <button class="add_field_buttonA">Adicionar</button>
+            <br /><br />
+            <div>
+              <label for="advreu">Advogado:</label
+              ><input type="text" name="mytext[]" /><br /><br />
+              <label for="oabadvreu">OAB:</label>
+              <input type="text name=oabadvreu" /><br /><br />
+              <label for="estoabreu">Estado:</label>
+              <select name="estoabreu" id="estoabreu">
                 <option value="branco"></option>
                 <option value="AC">AC</option>
                 <option value="AL">AL</option>
@@ -127,80 +203,83 @@
                 <option value="SP">SP</option>
                 <option value="SE">SE</option>
                 <option value="TO">TO</option>
-              </select></td>
-          </tr>
-          <tr>
-            <td><label for="reu">Réu:</label></td>
-            <td><input type="text" id="reu1" name="reu1"></td>
-          </tr>
-          <tr>
-            <td><label for="advogado3">Advogado:</label></td>
-            <td><input type="text" id="advogado4" name="advogado4"></td>
-          </tr>
-          <tr>
-            <td><label for="autor">OAB:</label></td>
-            <td><input type="text" id="oab4" name="oab4"></td>
-          </tr>
-          <tr>
-            <td><label for="valor">Valor da causa:</label></td>
-            <td><input type="text" id="valor" name="valor"></td>
-          </tr>
-          <tr>
-            <td><label for="recurso">Tipo de decisão:</label></td>
-            <td><select name="recurso" id="recurso">
-              <?php $counter1=-1;  if( isset($tipo_decisao) && ( is_array($tipo_decisao) || $tipo_decisao instanceof Traversable ) && sizeof($tipo_decisao) ) foreach( $tipo_decisao as $key1 => $value1 ){ $counter1++; ?>
-                <option value="<?php echo htmlspecialchars( $value1["tp_value"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["tp_desc"], ENT_COMPAT, 'UTF-8', FALSE ); ?></option>
-              <?php } ?>
-              </select></td>
-          </tr>
-          <tr>
-            <td><label for="tipo_peca">Peça a produzir:</label></td>
-            <td><select name="recurso" id="recurso">
-              <?php $counter1=-1;  if( isset($peca_produzir) && ( is_array($peca_produzir) || $peca_produzir instanceof Traversable ) && sizeof($peca_produzir) ) foreach( $peca_produzir as $key1 => $value1 ){ $counter1++; ?>  
-              <option value="<?php echo htmlspecialchars( $value1["pec_value"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["pec_desc"], ENT_COMPAT, 'UTF-8', FALSE ); ?></option>
-              <?php } ?>  
-              </select></td>
-          <tr>
-            <td><label for="inicio_prazo">Início do Prazo:</label></td>
-            <td><input type="date" id="datepicker" name="início" size="8" maxlength="10"></td>
-          </tr>
-          <tr>
-            <td><label for="dias_prazo">Prazo:</label></td>
-            <td><input type="text" id="dias_prazo" name="dias_prazo" size="2" maxlength="2"> <input type="radio" id="corridos" name="yesno" value="corridos">
-              <label for="corridos">Dias corridos</label>
-              <input type="radio" id="uteis" name="yesno" value="uteis">
-              <label for="corridos">Dias úteis</label><br></td>
-          </tr>
-          <tr>
-            <td><label for="prazo_final">Data final do prazo:</label></td>
-            <td><input type="date" id="datepicker" name="início" size="8" maxlength="10"></td>
-          </tr>
-          <tr>
-            <td><label for="custas">Há pagamento de custas?</label></td>
-  
-            <td><input type="radio" onclick="show1();" name="yesno" id="yesCheck" value="sim">Sim
-              <input type="radio" onclick="show2();" name="yesno" id="noCheck" value="nao">Não</td>
-          </tr>
-  
-          <tr class="hide" id="tr1">
-            <td><label for="custas" class>Valor a pagar:</label></td>
-            <td><input type="text" id="custas" name="custas"></td>
-          </tr>
-  
-          <tr class="hide" id="tr2">
-          </tr>
-        </table>
+              </select>
+            </div>
+          </div>
+          <br /><br />
+
+          <label for="recurso">Tipo de decisão:</label>
+          <select name="recurso" id="recurso">
+            <?php $counter1=-1;  if( isset($tipo_decisao) && ( is_array($tipo_decisao) || $tipo_decisao instanceof Traversable ) && sizeof($tipo_decisao) ) foreach( $tipo_decisao as $key1 => $value1 ){ $counter1++; ?>
+            <option value="<?php echo htmlspecialchars( $value1["tp_value"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["tp_desc"], ENT_COMPAT, 'UTF-8', FALSE ); ?></option>
+            <?php } ?>
+          </select>
+          <br /><br />
+          <label for="tipo_peca">Peça a produzir:</label>
+          <select name="recurso" id="recurso">
+            <?php $counter1=-1;  if( isset($peca_produzir) && ( is_array($peca_produzir) || $peca_produzir instanceof Traversable ) && sizeof($peca_produzir) ) foreach( $peca_produzir as $key1 => $value1 ){ $counter1++; ?>
+            <option value="<?php echo htmlspecialchars( $value1["pec_value"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["pec_desc"], ENT_COMPAT, 'UTF-8', FALSE ); ?></option>
+            <?php } ?>
+          </select>
+          <br /><br />
+          <label for="inicio_prazo">Início do Prazo:</label>
+          <input
+            type="date"
+            id="datepicker"
+            name="inicio"
+            size="8"
+            maxlength="10"
+          />
+          <br /><br />
+          <label for="dias_prazo">Prazo:</label>
+          <input
+            type="text"
+            id="dias_prazo"
+            name="dias_prazo"
+            size="2"
+            maxlength="2"
+          />
+          <input type="radio" id="corridos" name="dias" value="corridos" />
+          Dias corridos
+          <input type="radio" id="uteis" name="dias" value="uteis" />
+          Dias úteis
+          <br /><br />
+          <label for="prazo_final">Fim do prazo:</label>
+          <input
+            type="date"
+            id="datepicker"
+            name="fim"
+            size="8"
+            maxlength="10"
+          />
+          <br /><br />
+          <label for="custas">Há custas?</label>
+          <input
+            type="radio"
+            onclick="show1();"
+            name="yesno"
+            id="yesCheck"
+            value="sim"
+          />Sim
+          <input
+            type="radio"
+            onclick="show2();"
+            name="yesno"
+            id="noCheck"
+            value="nao"
+          />Não <br /><br />
+          <div class="hide" id="div1">
+            <label for="custas" class>Valor a pagar:</label>
+            <input type="text" id="custas" name="custas" />
+          </div>
+          <div class="hide" id="div2"></div>
+        </div>
       </div>
-      <div class="conteudo">
-        <label for="conteúdo">Conteúdo da nota de expediente:</label><br>
-        <textarea id="conteudo" name="conteudo" rows="44" cols="30"><?php echo htmlspecialchars( $summons["pub_conteudo"], ENT_COMPAT, 'UTF-8', FALSE ); ?></textarea>
+      <div class="btn" align="center">
+        <input type="reset" name="b1" value="Limpar" />
+        <input type="submit" name="b2" value="Enviar" />
       </div>
-      <br><br>
-      <div align="center">
-        <input type="reset" name="b1" value="Limpar">
-        <input type="submit" name="b2" value="Enviar">
-      </div>
-  </form>
-  <script src="../res/script/script.js"></script>
-</body>
+    </form>
+    <script src="./res/script/script2.js"></script>
+  </body>
 </html>
